19a117ddfe8d2c4b528390a8b21f00d0c814123d
joshuamcwilliams <jtmacnb@gmail.com>
2013-11-15 10:01:24 -0600

    initial commit

---
+++ WeatherDemo/StepCounterViewController.h
@@ -0,0 +1,18 @@
+//
+//  StepCounterViewController.h
+//  WeatherDemo
+//
+//  Created by Joshua McWilliams on 11/14/13.
+//  Copyright (c) 2013 Pebble Technology Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <PebbleKit/PebbleKit.h>
+#import <CoreMotion/CoreMotion.h>
+
+@interface StepCounterViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *totalStepsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *pebbleStatusLabel;
+- (void)setTargetWatch:(PBWatch*)watch;
+- (IBAction)refreshSteps:(id)sender;
+@end
---
+++ WeatherDemo/WDAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  WDAppDelegate.h
+//  WeatherDemo
+//
+//  Created by Martijn The on 2/7/13.
+//  Copyright (c) 2013 Pebble Technology Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
